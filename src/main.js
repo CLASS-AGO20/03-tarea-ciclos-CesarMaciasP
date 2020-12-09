2e14e17f5a0ec16df126c9d2591321587ebdef95
CesarMaciasP <cmacias@ucol.mx>
2020-12-09 14:08:09 -0600

    added sumatoriaSerieUno()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-export default class App {}
+export default class App {
+
+    sumatoriaSerieUno(numero) {
+
+        for(numero = numero;numero < 10;numero++) {
+            
+        }
+
+        return numero
+    }
+}
 
 let app = new App();
+
+console.log(app.sumatoriaSerieUno(2))
